c0fddb5fb8bc6a2542455d4b091158b28e2eb880
Alex Graf <alextgraf@gmail.com>
2021-03-26 17:09:35 -0700

    nothing useful

--- TopicTool/routes.py
+++ TopicTool/routes.py
@@ -3,5 +3,10 @@ from flask import render_template, url_for, redirect
 from TopicTool import app
 
 @app.route("/")
+@app.route("/index.html")
 def index():
     return render_template("index.html")
+
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
